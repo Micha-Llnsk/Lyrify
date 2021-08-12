fd36793e08a2b9972bb3e39843cd073bc4b84bfd
Michael Lulinski <michael.lulinski@gmx.de>
2021-08-12 11:45:16 +0200

    Add font

--- src/pages/Artists.js
+++ src/pages/Artists.js
@@ -2,6 +2,12 @@ export default function Artists() {
   return (
     <div>
       <h2>Artists</h2>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi
+        voluptatum numquam minus molestias repudiandae! Laboriosam ut quia,
+        dicta dolorum ducimus quod quam quasi quidem tenetur, facere facilis
+        animi sed explicabo!
+      </p>
     </div>
   );
 }
--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -2,6 +2,12 @@ export default function Favorites() {
   return (
     <div>
       <h2>Favorites</h2>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi
+        voluptatum numquam minus molestias repudiandae! Laboriosam ut quia,
+        dicta dolorum ducimus quod quam quasi quidem tenetur, facere facilis
+        animi sed explicabo!
+      </p>
     </div>
   );
 }
