52810b04883e04aa695f86a245bfcfa9926046e7
Michael Lulinski <michael.lulinski@gmx.de>
2021-08-12 11:21:20 +0200

    Add Footer, with icons and color

--- src/App.js
+++ src/App.js
@@ -9,30 +9,32 @@ import Footer from "./components/Footer.js";
 function App() {
   return (
     <Router>
-      <div className="header"></div>
-      <main className="content">
-        <Switch>
-          <Route path="/favorites">
-            <Favorites />
-          </Route>
-          <Route path="/artists">
-            <Artists />
-          </Route>
-          <Route path="/search">
-            <Search />
-          </Route>
-          <Route path="/playlists">
-            <Playlists />
-          </Route>
-          <Route exact path="/">
-            <Playlists />
-          </Route>
-          <Route path="*">
-            <h2>No match</h2>
-          </Route>
-        </Switch>
-      </main>
-      <Footer />
+      <div className="App">
+        <div className="header"></div>
+        <main className="content">
+          <Switch>
+            <Route path="/favorites">
+              <Favorites />
+            </Route>
+            <Route path="/artists">
+              <Artists />
+            </Route>
+            <Route path="/search">
+              <Search />
+            </Route>
+            <Route path="/playlists">
+              <Playlists />
+            </Route>
+            <Route exact path="/">
+              <Playlists />
+            </Route>
+            <Route path="*">
+              <h2>No match</h2>
+            </Route>
+          </Switch>
+        </main>
+        <Footer />
+      </div>
     </Router>
   );
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,26 @@
 import { NavLink } from "react-router-dom";
 import "./Footer.css";
+import library from "../icons/Library.svg";
+import artists from "../icons/Artists.svg";
+import bookmark from "../icons/Saved.svg";
+import search from "../icons/Search.svg";
 
 export default function Footer() {
   return (
     <footer className="footer">
       <nav className="navigation">
-        <NavLink to="/playlists">Playlists</NavLink>
-        <NavLink to="/search">Search</NavLink>
-        <NavLink to="/artists">Artists</NavLink>
-        <NavLink to="/favorites">Favorites</NavLink>
+        <NavLink to="/playlists">
+          <img className="Nav--icon" src={library} alt="Playlists" />
+        </NavLink>
+        <NavLink to="/search">
+          <img className="Nav--icon" src={search} alt="Search" />
+        </NavLink>
+        <NavLink to="/artists">
+          <img className="Nav--icon" src={artists} alt="Artists" />
+        </NavLink>
+        <NavLink to="/favorites">
+          <img className="Nav--icon" src={bookmark} alt="Favorites" />
+        </NavLink>
       </nav>
     </footer>
   );
