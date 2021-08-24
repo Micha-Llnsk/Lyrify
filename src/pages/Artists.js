4ac4b34ac1818c9fe2e81b5f874292907fdd38bd
Michael Lulinski <michael.lulinski@gmx.de>
2021-08-24 10:19:56 +0200

    fix artists page

--- src/pages/Artists.js
+++ src/pages/Artists.js
@@ -4,20 +4,25 @@ import useToken from "../hooks/useToken";
 export default function Artists() {
   const [artists, setArtists] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
 
   const [token] = useToken();
 
   useEffect(() => {
     setIsLoading(true);
-    fetch("https://api.spotify.com/v1/me/following", {
+    setIsError(false);
+    fetch("https://api.spotify.com/v1/me/following?type=artist", {
       headers: {
         Authorization: `Bearer ${token}`,
       },
     })
       .then((res) => res.json())
       .then((data) => {
-        setArtists(data.artists);
+        setArtists(data.artists.items);
         setIsLoading(false);
+      })
+      .catch(() => {
+        setIsError(true);
       });
   }, [token]);
 
@@ -27,8 +32,8 @@ export default function Artists() {
     }
     const ListOfArtists = artists.map((artist) => {
       return (
-        <li key={artist.items.id}>
-          <p>{artist.items.name}</p>
+        <li key={artist.id}>
+          <p>{artist.name}</p>
         </li>
       );
     });
