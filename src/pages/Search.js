876928c6b1c77a014d7c6c268c2adebd435172f3
Michael Lulinski <michael.lulinski@gmx.de>
2021-08-25 12:22:40 +0200

    Add search for artists and songs

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,12 +1,60 @@
+import { useState, useEffect } from "react";
+import useToken from "../hooks/useToken";
+
 export default function Search() {
+  const [search, setSearch] = useState("");
+  const [songs, setSongs] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [token] = useToken();
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(`https://api.spotify.com/v1/search?q=${search}&type=track%2Cartist`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setSongs(data.tracks.items);
+        setIsLoading(false);
+        console.log(data);
+      });
+  }, [search, token]);
+
+  function renderSongs() {
+    if (isLoading || songs === null) {
+      return "Loading...";
+    }
+    const ListOfSongs = songs.map((song) => {
+      return (
+        <li key={song.id}>
+          <img src={song.album.images[2].url} alt={song.album.name} />
+          <p>Artists: {song.artists[0].name}</p>
+          <p>Song: {song.name}</p>
+          <p>Album: {song.album.name}</p>
+        </li>
+      );
+    });
+    return ListOfSongs;
+  }
+
+  function handleChange(e) {
+    const value = e.target.value;
+    setSearch(value);
+  }
+
   return (
     <div>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi
-        voluptatum numquam minus molestias repudiandae! Laboriosam ut quia,
-        dicta dolorum ducimus quod quam quasi quidem tenetur, facere facilis
-        animi sed explicabo!
-      </p>
+      <form className="search" onChange={handleChange}>
+        <input
+          type="search"
+          placeholder="Search Artists/Songs"
+          name="search"
+          id="search"
+        />
+      </form>
+      <ul className="Songlist">{renderSongs()}</ul>
     </div>
   );
 }
