cb6831b9b140a7cac09a7fee14ec8d2384b49bce
Michael Lulinski <michael.lulinski@gmx.de>
2021-09-02 13:24:54 +0200

    Add server, Add lyrics-finder library, Add fetch lyrics

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+const lyricsFinder = require("lyrics-finder");
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+app.get("/api/lyrics", async (req, res) => {
+  const artist = req.query.artist;
+  const track = req.query.track;
+
+  const lyrics = (await lyricsFinder(artist, track)) || "No Lyrics Found";
+  res.json({ lyrics });
+});
+
+app.listen(4000, () => {
+  console.log("Listening on http://localhost:4000");
+});
